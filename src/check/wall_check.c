4b93785da9fcb15ca85230ffab38b396bf037ead
bertt6 <xcushy1@gmail.com>
2023-06-13 22:13:01 +0300

    Fixed

--- src/check/wall_check.c
+++ src/check/wall_check.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   wall_check.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yugurlu <yugurlu@student.42.fr>            +#+  +:+       +#+        */
+/*   By: macos <macos@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 16:05:35 by yugurlu           #+#    #+#             */
-/*   Updated: 2023/06/12 15:06:41 by yugurlu          ###   ########.fr       */
+/*   Updated: 2023/06/13 22:12:36 by macos            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,33 +62,40 @@ int	line_check(t_cub3d *cub3d)
 	return (0);
 }
 
+int map_height(t_cub3d *cub3d)
+{
+	int i = 0;
+	while(cub3d->map.map[i])
+		i++;
+	return i;
+}
+
+int is_okey(t_cub3d *cub3d, int i, int j)
+{
+	if(j - 1 < 0 || j + 1 > (int)ft_strlen(cub3d->map.map[i]) || i + 1 > map_height(cub3d) || i - 1 < 0)
+		return 1;
+	if(cub3d->map.map[i - 1][j] == ' ' || cub3d->map.map[i + 1][j] == ' ' || cub3d->map.map[i][j + 1] == ' ' || cub3d->map.map[i][j - 1] == ' ')
+		return 1;
+	return 0;
+}
+
 int	wall_check(t_cub3d *cub3d)
 {
-	int	i;
-	int	j;
-
-	i = -1;
-	while (cub3d->map.map[++i])
+	int i = 0;
+	int j = 0;
+	while(cub3d->map.map[i])
 	{
 		j = 0;
-		if (i == 0 || !cub3d->map.map[i + 1])
+		while(cub3d->map.map[i][j])
 		{
-			while (cub3d->map.map[i][j])
+			if(cub3d->map.map[i][j] == '0')
 			{
-				if (cub3d->map.map[i][j] != '1')
-					return (1);
-				j++;
+				if(is_okey(cub3d, i, j))
+					return 1;
 			}
+			j++;
 		}
-		else
-		{
-			if (cub3d->map.map[i][0] != '1')
-				return (1);
-			while (cub3d->map.map[i][j])
-				j++;
-			if (cub3d->map.map[i][j - 1] != '1')
-				return (1);
-		}
+		i++;
 	}
-	return (0);
+	return 0;
 }
